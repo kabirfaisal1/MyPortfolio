6985ccaa48500b63d86ffbb213771e71804285b5
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-10-05 22:21:17 -0400

    Good to submit

--- README.md
+++ README.md
@@ -32,3 +32,5 @@ WHEN I resize the page or view the site on various screens and devices
 THEN I am presented with a responsive layout that adapts to my viewport
 ```
 
+# App Screenshots
+![Image at homepage.](./assests/images/MyProfile.png)
--- assests/js/jsScript.js
+++ assests/js/jsScript.js
@@ -1,25 +1,38 @@
 var searchResults = document.getElementById("result");
+var searchInput = document.getElementById("searchInput");
 
-var search_terms = ['About Me', 'Projects', 'Skill', 'Work', 'Projects'];
-
+var search_terms = ['About Me','Projects', 'Skill', 'Work','Projects'];
+var item;
 function autocompleteMatch(input) {
     if (input == '') {
       return [];
     }
-    var reg = new RegExp(input)
+    var reg = new RegExp(input);
     return search_terms.filter(function(term) {
         if (term.match(reg)) {
-          return term;
+            return term;
         }
     });
   }
+
   function showResults(val) {
-  
+
     searchResults.innerHTML = '';
     let list = '';
     let terms = autocompleteMatch(val);
     for (i=0; i<terms.length; i++) {
-      list += '<li>' + terms[i] + '</li>';
+        if(terms[i].toLowerCase.match(val)){
+            searchResults.style.setProperty("visibility", "visible")
+            list += '<li>' + terms[i] + '</li>';
+        }else{
+            searchResults.style.setProperty("visibility", "hidden")
+        }
     }
-    res.innerHTML = '<ul>' + list + '</ul>';
+    searchResults.innerHTML = '<ul>' + list + '</ul>';
   }
+
+  searchResults.addEventListener("click", ()=>{
+    searchInput.textContent = item ;
+    searchResults.style.setProperty("visibility", "hidden");
+    
+  })
